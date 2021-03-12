8617aa7a97e9af16fd7ceadab87d973ec0a48b6f
Jim Clark <jim@atomist.com>
2021-03-11 22:53:28 -0800

    put Dockerfiles in separate dirs

--- build-ecr.sh
+++ build-ecr.sh
@@ -4,12 +4,12 @@ docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=Dockerfile.base \
   -t "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest" \
-  -f Dockerfile.base .
+  -f base/Dockerfile .
 docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=Dockerfile.service \
   -t "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service:latest" \
-  -f Dockerfile.service .
+  -f service/Dockerfile .
 aws ecr get-login-password --region us-east-1 --profile sts | docker login --username AWS --password-stdin 111664719423.dkr.ecr.us-east-1.amazonaws.com
 docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest"
 docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service:latest"
